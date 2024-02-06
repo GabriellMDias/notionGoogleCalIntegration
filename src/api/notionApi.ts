a1696d85520c28cbfc57d4389e7bf925fa1207c3
Gabriel Dias <gabrielmoreiradias270402@gmail.com>
2024-02-06 20:46:58 -0300

    Add getEvents and insertEvent endpoints

--- src/api/notionApi.ts
+++ src/api/notionApi.ts
@@ -1,12 +1,63 @@
-const { Client } = require('@notionhq/client');
+import { Client } from '@notionhq/client';
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
+const DATABASE_ID = process.env.CALENDAR_DATABASE_ID || "";
 
-export const getEvents = async () => {
-  const databaseId = 'bc6e4c91-1618-49a6-86a6-628ba676acad';
+const getEventsLastTenMin = async () => {
+  const databaseId = DATABASE_ID;
+
+  const now = new Date()
+  const tenMinutesAgo = new Date(now.getTime() - (10 * 60000));
   
   const response = await notion.databases.query({
     database_id: databaseId,
+    filter: {
+      timestamp: "last_edited_time",
+      last_edited_time: {
+        after: tenMinutesAgo.toISOString()
+      }
+    }
   });
-  console.log(response);
+  return response;
+}
+
+const insertEvent = async (title:string, iCalUID: string, dateStart: string, dateEnd?: string) => {
+  const response = await notion.pages.create({
+    parent: {
+        type: "database_id",
+        database_id: DATABASE_ID
+    },
+    properties: {
+        Name: {
+            title: [
+                {
+                    text: {
+                        content: title
+                    }
+                }
+            ]
+        },
+        Deadline:{
+          date: {
+            start: dateStart,
+            end: dateEnd ?? null
+          }
+        },
+        iCalUID: {
+          rich_text: [
+            {
+                text: {
+                    content: iCalUID
+                }
+            }
+        ]
+        }
+    },
+});
+  return response;
+}
+
+export default {
+  getEventsLastTenMin,
+  insertEvent
 }
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 dotenv.config();
-import { getEvents } from "./api/notionApi";
-
-
+import notionApi from "./api/notionApi";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", async (req: Request, res: Response) => {
-  await getEvents()
-  res.send("Express + TypeScript Server");
+app.use(express.json());
+
+app.get("/getEventsLastTenMin", async (req: Request, res: Response) => {
+  const result = await notionApi.getEventsLastTenMin()
+  res.send(result);
 });
+
+app.post("/insertEvent", async (req: Request, res: Response) => {
+  const {title, dateStart, dateEnd, iCalUID} = req.body
+
+  const result = await notionApi.insertEvent(title, iCalUID, dateStart, dateEnd)
+  res.send(result)
+})
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
